5102c189a38e8f690e358152bb0df3a7a9aa8b79
JinGoo <zzgg3040@naver.com>
2025-01-21 16:44:17 +0900

    chore: apply google-format

--- src/main/java/com/example/frontend4backendbasic/Frontend4backendbasicApplication.java
+++ src/main/java/com/example/frontend4backendbasic/Frontend4backendbasicApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Frontend4backendbasicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Frontend4backendbasicApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(Frontend4backendbasicApplication.class, args);
+  }
 }
--- src/main/java/com/example/frontend4backendbasic/NoParameterAjaxRestController.java
+++ src/main/java/com/example/frontend4backendbasic/NoParameterAjaxRestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NoParameterAjaxRestController {
 
-    @RequestMapping("/get-with-no-parameter")
-    public String getWithNoParameter() {
-        return "파라미터가 없는 GET 요청";
-    }
+  @RequestMapping("/get-with-no-parameter")
+  public String getWithNoParameter() {
+    return "파라미터가 없는 GET 요청";
+  }
 }
